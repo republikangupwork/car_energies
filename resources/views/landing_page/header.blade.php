cee95199eea74c24f24c82a434466e356a54f23a
republikangupwork <webcareer1@gmail.com>
2019-09-29 14:49:06 +0800

    meta

--- resources/views/landing_page/header.blade.php
+++ resources/views/landing_page/header.blade.php
@@ -3,7 +3,8 @@
 
 <head>
     <!-- Required meta tags -->
-    <meta http-equiv="Content-Security-Policy" content="connect-src 'self' data:; "/>
+    <meta http-equiv="Content-Security-Policy"
+    content="connect-src geodata.solutions 'self'">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
