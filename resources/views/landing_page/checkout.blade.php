ad3365350197377b5ca3def1d8338210c56e1dff
fred.unciano <fred.unciano@acacia-soft.com>
2019-10-02 20:36:00 +0800

    a

--- resources/views/landing_page/checkout.blade.php
+++ resources/views/landing_page/checkout.blade.php
@@ -181,82 +181,62 @@
 @include('landing_page.footer')
       <script>
         // Example starter JavaScript for disabling form submissions if there are invalid fields
-        var stripe = Stripe('pk_test_wrvJOlwyyqQew5S1wlk8n3c90083TVuaIs');
-        var elements = stripe.elements();
+//         var stripe = Stripe('pk_test_wrvJOlwyyqQew5S1wlk8n3c90083TVuaIs');
+//         var elements = stripe.elements();
 
-        // Custom styling can be passed to options when creating an Element.
-var style = {
-  base: {
-    // Add your base input styles here. For example:
-    fontSize: '16px',
-    color: "#32325d",
-  }
-};
+//         // Custom styling can be passed to options when creating an Element.
+// var style = {
+//   base: {
+//     // Add your base input styles here. For example:
+//     fontSize: '16px',
+//     color: "#32325d",
+//   }
+// };
 
-// Create an instance of the card Element.
-var card = elements.create('card', {style: style});
+// // Create an instance of the card Element.
+// var card = elements.create('card', {style: style});
 
-// Add an instance of the card Element into the `card-element` <div>.
-card.mount('#card-element');
+// // Add an instance of the card Element into the `card-element` <div>.
+// card.mount('#card-element');
 
-card.addEventListener('change', function(event) {
-  var displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
-});
+// card.addEventListener('change', function(event) {
+//   var displayError = document.getElementById('card-errors');
+//   if (event.error) {
+//     displayError.textContent = event.error.message;
+//   } else {
+//     displayError.textContent = '';
+//   }
+// });
 
-var form = document.getElementById('payment-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
+// var form = document.getElementById('payment-form');
+// form.addEventListener('submit', function(event) {
+//   event.preventDefault();
 
-  stripe.createToken(card).then(function(result) {
-    if (result.error) {
-      // Inform the customer that there was an error.
-      var errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
-    } else {
-      // Send the token to your server.
-      stripeTokenHandler(result.token);
-    }
-  });
-});
+//   stripe.createToken(card).then(function(result) {
+//     if (result.error) {
+//       // Inform the customer that there was an error.
+//       var errorElement = document.getElementById('card-errors');
+//       errorElement.textContent = result.error.message;
+//     } else {
+//       // Send the token to your server.
+//       stripeTokenHandler(result.token);
+//     }
+//   });
+// });
 
-function stripeTokenHandler(token) {
-  console.log(token);
-  // Insert the token ID into the form so it gets submitted to the server
-  var form = document.getElementById('payment-form');
-  var hiddenInput = document.createElement('input');
-  hiddenInput.setAttribute('type', 'hidden');
-  hiddenInput.setAttribute('name', 'stripeToken');
-  hiddenInput.setAttribute('value', token.id);
-  form.appendChild(hiddenInput);
+// function stripeTokenHandler(token) {
+//   console.log(token);
+//   // Insert the token ID into the form so it gets submitted to the server
+//   var form = document.getElementById('payment-form');
+//   var hiddenInput = document.createElement('input');
+//   hiddenInput.setAttribute('type', 'hidden');
+//   hiddenInput.setAttribute('name', 'stripeToken');
+//   hiddenInput.setAttribute('value', token.id);
+//   form.appendChild(hiddenInput);
 
-  // Submit the form
-  form.submit();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//   // Submit the form
+//   form.submit();
+// }
 
 
       (function() {
